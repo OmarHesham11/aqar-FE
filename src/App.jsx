a759ffa5b1f4f64b1daf31aea6abc1ae4c7d0e6f
Habosa162 <arabxbyu@premium250.web-hosting.com>
2024-11-14 07:35:31 +0200

    AppBar+Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import './App.css'
+import './App.css';
+import  Home  from './Pages/Home';
+import  About  from './Pages/About';
+import  Construction  from './Pages/Construction';
 import Layout from './Components/layout';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
-
+  const router = createBrowserRouter([
+    {
+      path: '', element: <Layout /> , 
+      children:[
+          { path: "/", element: <Home /> },
+          { path: "/about", element: <About /> },
+          { path: "/construction", element: <Construction /> }
+        ]
+    }
+  ]);
   return (
     <>
-      <Layout />
+      <RouterProvider router={router}>
+ 
+      </RouterProvider>
+
     </>
   )
 }
--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,12 +1,12 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-
+import {Outlet} from 'react-router-dom'
 
 function Layout() {
     return (
         <>
             <Navbar />
-                <h1>Layout</h1>
+                <Outlet></Outlet> 
             <Footer />
         </>
     )
